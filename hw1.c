8aac610a15bd14a584c6a34d990cee962c34d2af
wlgnssla <145082248+wlgnssla@users.noreply.github.com>
2023-09-17 16:12:11 +0900

    c프로그래밍 과제1

---
+++ hw1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void) {
+	
+	int num1, num2;
+	printf("input two integers:");
+	scanf("%d %d", &num1, &num2);
+	printf("%d & %d = %d\n", num1, num2, num1 & num2);
+	printf("%d | %d = %d\n", num1, num2, num1 | num2);
+	printf("%d ^ %d = %d\n", num1, num2, num1 ^ num2);
+
+	return 0;
+
+
+
+
+
+
+}
