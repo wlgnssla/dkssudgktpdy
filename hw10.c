c69321e64f08a76c71aafbd79df3146ca3a0c6b7
wlgnssla <145082248+wlgnssla@users.noreply.github.com>
2023-12-07 15:08:16 +0900

    hw10.c

--- hw10.c
+++ hw10.c
@@ -5,27 +5,30 @@ typedef struct {
     char name[20];
     char country[20];
     int population;
-} infolist;
+} cityInfo; 
 
 int main() {
-    infolist list[3];
+    cityInfo cities[3]; 
 
     printf("Input three cities:\n");
     for (int i = 0; i < 3; i++) {
-        printf("Name: ");
-        fgets(list[i].name, sizeof(list[i].name), stdin);
 
-        printf("Country: ");
-        fgets(list[i].country, sizeof(list[i].country), stdin);
+        printf("Name> ");
+        fgets(cities[i].name, sizeof(cities[i].name), stdin);
+        cities[i].name[strcspn(cities[i].name, "\n")] = 0;
 
-        printf("Population: ");
-        scanf("%d", &list[i].population);
-        getchar();
+        printf("Country> ");
+        fgets(cities[i].country, sizeof(cities[i].country), stdin);
+        cities[i].country[strcspn(cities[i].country, "\n")] = 0;
+
+        printf("Population> ");
+        scanf("%d", &cities[i].population);
+        getchar(); 
     }
 
     printf("Printing the three cities:\n");
     for (int i = 0; i < 3; i++) {
-        printf("%d. %s in %s with a population of %d people\n", i + 1, list[i].name, list[i].country, list[i].population);
+        printf("%d. %s in %s with a population of %d people\n", i + 1, cities[i].name, cities[i].country, cities[i].population);
     }
 
     return 0;
